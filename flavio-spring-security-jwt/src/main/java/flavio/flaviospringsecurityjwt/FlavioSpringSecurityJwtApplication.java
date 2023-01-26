b0554ffa58917178cf0cc9c545ca26de524cfd03
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-26 17:23:39 -0300

    Create new project with spring security and jwt

---
+++ flavio-spring-security-jwt/src/main/java/flavio/flaviospringsecurityjwt/FlavioSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package flavio.flaviospringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavioSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavioSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ flavio-spring-security-jwt/src/test/java/flavio/flaviospringsecurityjwt/FlavioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package flavio.flaviospringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
